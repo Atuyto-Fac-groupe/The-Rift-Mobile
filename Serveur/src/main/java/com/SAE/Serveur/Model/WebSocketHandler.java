eb59c2e0d7f3fcebb9e673240e252b7217625270
Alexandre <alexandresoune@gmail.com>
2024-09-25 15:12:38 +0200

    création de la configuration android

--- Serveur/src/main/java/com/SAE/Serveur/Model/WebSocketHandler.java
+++ Serveur/src/main/java/com/SAE/Serveur/Model/WebSocketHandler.java
@@ -48,17 +48,13 @@ public class WebSocketHandler extends TextWebSocketHandler {
     protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 
         System.out.println("Message brut reçu du client : " + message.getPayload());
-
-        // Parse le message reçu (JSON)
         JSONObject jsonMessage = new JSONObject(message.getPayload());
-        String from = jsonMessage.getString("from");         // Expéditeur
-        String to = jsonMessage.getString("to");             // Destinataire
-        String messageText = jsonMessage.getString("message");  // Texte du message
+        String from = jsonMessage.getString("from");
+        String to = jsonMessage.getString("to");
+        String messageText = jsonMessage.getString("message");
 
-        // Récupérer la session WebSocket du destinataire
         WebSocketSession recipientSession = this.session.get(to);
 
-        // Vérifier si la session du destinataire est ouverte et envoyer le message
         if (recipientSession != null && recipientSession.isOpen()) {
             recipientSession.sendMessage(new TextMessage("Message de " + from + " : " + messageText));
         } else {
