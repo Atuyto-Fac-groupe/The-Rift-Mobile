a002868a33f76566e161580d647cb7275dbf91db
Emeline <66369599+Emeline1710@users.noreply.github.com>
2024-10-18 08:54:16 +0200

    Vérification de l'emplacement des pièces

--- Quest/Assets/SnapObject.cs
+++ Quest/Assets/SnapObject.cs
@@ -4,6 +4,9 @@ using System.Collections.Generic;
 public class SnapObject : MonoBehaviour
 {
     public List<Transform> snapZones; // Liste des zones de snap
+    public List<Transform> puzzlePieces; // Liste des pièces du puzzle
+    public Transform plateau;
+    public GameObject plateauTermine;
     private bool isSnapped = false;
     private Transform currentSnapZone;
 
@@ -27,6 +30,7 @@ public class SnapObject : MonoBehaviour
             {
                 currentSnapZone = closestSnapZone;
                 isSnapped = true;
+                CheckPuzzleCompletion();
             }
         }
     }
@@ -57,5 +61,37 @@ public class SnapObject : MonoBehaviour
         }
 
         return closestSnapZone;
+    }
+
+    void CheckPuzzleCompletion()
+    {
+        bool allCorrect = true;
+
+        for (int i = 0; i < puzzlePieces.Count; i++)
+        {
+            SnapObject pieceScript = puzzlePieces[i].GetComponent<SnapObject>();
+            if (pieceScript.currentSnapZone != snapZones[i])
+            {
+                allCorrect = false;
+                break;
+            }
+        }
+
+        if (allCorrect)
+        {
+            // Si toutes les pièces sont à leur bonne place, faire disparaître le plateau
+            DisappearPuzzle();
+        }
+    }
+
+    void DisappearPuzzle()
+    {
+        // Faire disparaître le plateau
+        plateau.gameObject.SetActive(false);
+        foreach (Transform piece in puzzlePieces)
+        {
+            piece.gameObject.SetActive(false);
+        }
+        plateauTermine.gameObject.SetActive(true);
     }
 }
