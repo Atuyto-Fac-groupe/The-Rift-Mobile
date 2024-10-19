67e1281f80a8f53d1b18a8d4126292b8930cae5a
alexa <alexandresoune@gmail..com>
2024-10-19 09:41:26 +0200

    Amélioration de l'algorithme de détection et calcule des posotions des différents router.
    Faire test réel IUT

--- Mobile/app/src/main/java/main/Controler/CartographieControler.java
+++ Mobile/app/src/main/java/main/Controler/CartographieControler.java
@@ -24,9 +24,8 @@ public class CartographieControler implements View.OnTouchListener, View.OnClick
     private CartographyActivity cartographyActivity;
     private WifiManager wifiManager;
     private Box<Router> routerBox;
-    private GridMap gridMap;
-    private Box<GridMap> gridMapBox;
-    private GridImageView gridImageView;
+//    private GridMap gridMap;
+//    private Box<GridMap> gridMapBox;
     private HashMap<Point, List<Router>> pointListe;
     private List<Router> topRouter;
     private List<Point> points;
@@ -36,16 +35,16 @@ public class CartographieControler implements View.OnTouchListener, View.OnClick
         this.cartographyActivity = cartographyActivity;
         this.wifiManager = this.cartographyActivity.getSystemService(WifiManager.class);
         this.routerBox = ObjectBox.get().boxFor(Router.class);
-        this.gridMapBox = ObjectBox.get().boxFor(GridMap.class);
+//        this.gridMapBox = ObjectBox.get().boxFor(GridMap.class);
         this.pointListe = new HashMap<>();
         this.topRouter = new ArrayList<>();
         this.points = new ArrayList<>();
-        this.gridMap = this.gridMapBox.query().equal(GridMap_.id, 1).build().findFirst();
-        if (this.gridMap == null) {
-            this.gridMap = new GridMap(App.ROW, App.COL);
-        }
-        this.gridMapBox.put(this.gridMap);
-        this.gridImageView = this.cartographyActivity.getBinding().gridView;
+//        this.gridMap = this.gridMapBox.query().equal(GridMap_.id, 1).build().findFirst();
+//        if (this.gridMap == null) {
+//            this.gridMap = new GridMap(App.ROW, App.COL);
+//        }
+//        this.gridMapBox.put(this.gridMap);
+//        this.gridImageView = this.cartographyActivity.getBinding().gridView;
         if (ActivityCompat.checkSelfPermission(this.cartographyActivity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this.cartographyActivity, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
         }
@@ -57,64 +56,40 @@ public class CartographieControler implements View.OnTouchListener, View.OnClick
 
     @Override
     public boolean onTouch(View v, MotionEvent event) {
-        List<Router> tmpRouter = new ArrayList<>();
-        List<ScanResult> topTreeRouterFromScan;
+        if (event.getAction() != MotionEvent.ACTION_DOWN) return false;
+        if (ActivityCompat.checkSelfPermission(this.cartographyActivity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+            return false;
 
-        float x;
-        float y;
-        int[] coordinatesCell;
-        if (event.getAction() == MotionEvent.ACTION_DOWN) {
-            if (ActivityCompat.checkSelfPermission(this.cartographyActivity, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                return false;
+        float x = event.getX();
+        float y = event.getY();
+        Point point = new Point(x, y);
+        this.cartographyActivity.getBinding().gridView.addWifiMarker(x, y);
+        this.wifiManager.startScan();
+        Set<Integer> uniqueLevels = new HashSet<>();
+        List<Router> tmpRouter = this.wifiManager.getScanResults().stream()
+                .sorted((r1, r2) -> Integer.compare(r2.level, r1.level))
+                .filter(scanResult -> uniqueLevels.add(scanResult.level))
+                .limit(3)
+                .map(s -> new Router(s.level, s.BSSID))
+                .collect(Collectors.toList());
+
+        for (Router s : tmpRouter) {
+            Router existingRouter = Router.getRouterFromBssid(s.getBssid());
+            if (existingRouter == null) {
+                long id = routerBox.put(s);
+                s.setId(id);
+                topRouter.add(s);
+            } else {
+                existingRouter.setLevel(s.getLevel());
+                topRouter.add(existingRouter);
             }
-            x = event.getX();
-            y = event.getY();
-            Point point = new Point(x, y);
-            this.cartographyActivity.getBinding().gridView.addWifiMarker(x, y);
-            this.wifiManager.startScan();
-            Set<Integer> uniqueLevels = new HashSet<>();
-            topTreeRouterFromScan = this.wifiManager.getScanResults().stream()
-                    .sorted((r1, r2) -> Integer.compare(r2.level, r1.level))
-                    .filter(scanResult -> uniqueLevels.add(scanResult.level))
-                    .limit(3)
-                    .collect(Collectors.toList());
-
-            topTreeRouterFromScan.forEach(s -> tmpRouter.add(new Router(s.level, s.BSSID)));
-
-            for (Router s : tmpRouter) {
-                Router router = Router.getRouterFromBssid(s.getBssid());
-                int oldLevel = s.getLevel();
-//                for (Router router1 : this.topRouter) {
-//                    if (router1.getBssid().equals(s.getBssid())) {
-//                        added = true;
-//                        break;
-//                    }
-//                }
-
-                if (router == null) {
-                    long id = routerBox.put(s);
-                    s.setId(id);
-//                    this.pointListe.put(s, new double[]{x, y});
-                    s.setLevel(oldLevel);
-                    topRouter.add(s);
-                } else {
-//                    this.pointListe.put(router, new double[]{x, y});
-                    router.setLevel(oldLevel);
-                    topRouter.add(router);
-                }
-                this.pointListe.put(point, tmpRouter);
-                this.points.add(point);
-            }
-
-
-//            coordinatesCell = Cell.convertToGrid(x, y, this.gridImageView.getWidth(), this.gridImageView.getHeight(), App.ROW, App.COL);
-//            this.gridMap.getGridCells()[coordinatesCell[0]][coordinatesCell[1]] = new Cell(coordinatesCell[0], coordinatesCell[1]);
-//            this.gridMap.save();
-
-            return true;
         }
-        return false;
+        this.pointListe.put(point, tmpRouter);
+        this.points.add(point);
+
+        return true;
     }
+
 
     @Override
     public void onClick(View view) {
@@ -131,21 +106,14 @@ public class CartographieControler implements View.OnTouchListener, View.OnClick
     private void calculatePositionRouter(Router router) {
         List<Point> topPoint = new ArrayList<>();
         List<Router> routerSelcted = new ArrayList<>();
-        Set<Integer> uniqueLevels = new HashSet<>();
-        int cpt = 0;
-
         for (Point p : this.points) {
-            if (cpt == 3) break;
-            List<Router> r = this.pointListe.get(p);
-            if (r == null) break;
-
-            for (Router routerSel : r) {
-                if (routerSel.getBssid().equals(router.getBssid()) && uniqueLevels.add(routerSel.getLevel())) {
-                    Router clonedRouter = new Router(routerSel.getLevel(), routerSel.getBssid());
-                    routerSelcted.add(clonedRouter);
-                    topPoint.add(p);
-                    cpt++;
-                }
+            List<Router> routerFromPoint = this.pointListe.get(p);
+            if (routerFromPoint == null) break;
+            Router clonedRouter = routerFromPoint.stream().filter(r -> r.getBssid().equals(router.getBssid()))
+                    .findFirst().orElse(null);
+            if (clonedRouter != null) {
+                routerSelcted.add(clonedRouter);
+                topPoint.add(p);
             }
         }
 
