888c4b9c990a0adddda39cebf737401c78ac1afd
alexa <alexandresoune@gmail..com>
2024-11-12 21:21:53 +0100

    ajout d'une page pour tester sans téléphone

--- Mobile/app/src/main/java/main/App.java
+++ Mobile/app/src/main/java/main/App.java
@@ -19,7 +19,7 @@ import java.util.Objects;
 
 public class App extends Application implements SocketObserver {
 
-    public static String WebSocketUrl = "ws://lamb-master-vulture.ngrok-free.app/ws?idpersonne=2";
+    public static String WebSocketUrl = "ws://lamb-master-vulture.ngrok-free.app:9001/ws?idpersonne=2";
 
     public static OnSocketListener socketListener;
 
