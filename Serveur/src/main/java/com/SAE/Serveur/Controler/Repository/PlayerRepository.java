21ca2077c10b8605fd247f6a581a5d4beecd85a4
alexa <alexandresoune@gmail..com>
2024-11-21 10:35:52 +0100

    modif git

--- Serveur/src/main/java/com/SAE/Serveur/Controler/Repository/PlayerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.SAE.Serveur.Controler.Repository;
-
-import com.SAE.Serveur.Model.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<Player, Long> {
-
-
-}
--- Serveur/src/main/java/com/SAE/Serveur/Controler/Repository/StapeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.SAE.Serveur.Controler.Repository;
-
-import com.SAE.Serveur.Model.Stape;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StapeRepository extends JpaRepository<Stape, Long> {
-}
