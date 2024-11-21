c0829dcb00cb6a3fc78fd60fb9e5faff6627eebc
alexa <alexandresoune@gmail..com>
2024-11-21 10:35:52 +0100

    modif git

--- Serveur/src/main/java/com/SAE/Serveur/Controler/Repository/ProgressRepository.java
+++
@@ -1,7 +0,0 @@
-package com.SAE.Serveur.Controler.Repository;
-
-import com.SAE.Serveur.Model.Progress;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProgressRepository extends JpaRepository<Progress, Long> {
-}
