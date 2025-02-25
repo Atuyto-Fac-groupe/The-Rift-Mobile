2c0ac28b0af487159faa57f7e24df2766604103a
Alexandre <alexandresoune@gmail.com>
2025-02-25 16:30:36 +0100

    modification import inutile

--- app/src/main/java/main/view/MainActivity.java
+++ app/src/main/java/main/view/MainActivity.java
@@ -1,6 +1,9 @@
 package main.view;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.therift.R;
